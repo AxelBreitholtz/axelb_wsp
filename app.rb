781caf42380a616a23387bdc5f1342c06b6971ff
AxelBreitholtzNTIJohanneberg <axel.breitholtz@elev.ga.ntig.se>
2024-02-28 16:31:40 +0100

    v.9

--- app.rb
+++ app.rb
@@ -7,6 +7,23 @@ get('/') do
     db = SQLite3::Database.new('db/fotboll.db')
     db.results_as_hash = true
     @result = db.execute("SELECT * FROM league")
-    p @result
-    slim(:"ligor/index", locals: {ligor:@result})
+    slim(:"ligor/index", locals: {results:@result})
+end
+
+get('/clubs/new') do 
+    slim(:"klubbar/new")
+end 
+
+post('/clubs/new') do 
+    klubbnamn = params[:klubbnamn]
+    rating = params[:rating].to_i
+    league_id = params[:league_id].to_i
+    db = SQLite3::Database.new('db/fotboll.db')
+    db.execute("INSERT INTO club (name,rating,league_id) VALUES (?,?,?)",klubbnamn,rating,league_id)
+end
+
+get('/clubs/index') do
+    db = SQLite3::Database.new('db/fotboll.db')
+    @result = db.execute("SELECT * FROM club")
+    slim(:"klubbar/index", locals: {results:@result})
 end
