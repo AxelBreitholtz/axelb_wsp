48faf68dd350a79e356efffaf9e236e0ae40b8f1
AxelBreitholtzNTIJohanneberg <axel.breitholtz@elev.ga.ntig.se>
2024-03-04 23:29:15 +0100

    v.10 (hemma)

--- app.rb
+++ app.rb
@@ -6,7 +6,7 @@ require 'sqlite3'
 get('/') do
     db = SQLite3::Database.new('db/fotboll.db')
     db.results_as_hash = true
-    @result = db.execute("SELECT * FROM league")
+    @result = db.execute("SELECT * FROM leagues ORDER BY leagues.id;")
     slim(:"ligor/index", locals: {results:@result})
 end
 
@@ -19,11 +19,22 @@ post('/clubs/new') do
     rating = params[:rating].to_i
     league_id = params[:league_id].to_i
     db = SQLite3::Database.new('db/fotboll.db')
-    db.execute("INSERT INTO club (name,rating,league_id) VALUES (?,?,?)",klubbnamn,rating,league_id)
+    db.execute("INSERT INTO clubs (club_name,rating) VALUES (?,?)",klubbnamn,rating)
+    club_Id = db.execute("SELECT id FROM clubs ORDER BY id DESC LIMIT 1;")
+    db.execute("INSERT INTO leaguesclubs (league_id,club_id) VALUES (?,?)",league_id,club_Id)
+    redirect '/clubs/new'
 end
 
-get('/clubs/index') do
+get('/clubs/index/:league_id') do
     db = SQLite3::Database.new('db/fotboll.db')
-    @result = db.execute("SELECT * FROM club")
+    db.results_as_hash = true
+    sql_code = "
+    SELECT clubs.club_name, clubs.rating FROM leagues
+    INNER JOIN leaguesclubs ON leagues.id = leaguesclubs.league_id
+    INNER JOIN clubs ON clubs.id = leaguesclubs.club_id
+    WHERE leagues.id = ?
+    ORDER BY clubs.rating DESC;
+    "
+    @result = db.execute(sql_code,[params[:league_id]])
     slim(:"klubbar/index", locals: {results:@result})
 end
