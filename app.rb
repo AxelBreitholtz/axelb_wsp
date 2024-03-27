5e427177113cc819895f902b5d88c41e14d799c4
AxelBreitholtzNTIJohanneberg <axel.breitholtz@elev.ga.ntig.se>
2024-03-27 15:33:06 +0100

    v.13

--- app.rb
+++ app.rb
@@ -2,6 +2,9 @@ require 'slim'
 require 'sinatra'
 require 'sinatra/reloader'
 require 'sqlite3'
+require 'bcrypt'
+
+enable :sessions
 
 get('/') do
     db = SQLite3::Database.new('db/fotboll.db')
@@ -26,9 +29,11 @@ post('/clubs/new') do
 end
 
 get('/clubs/index') do
+    id = session[:id].to_i
     db = SQLite3::Database.new('db/fotboll.db')
     db.results_as_hash = true
-    @result = db.execute("SELECT * FROM clubs ORDER BY club_name ASC;")
+    @result = db.execute("SELECT * FROM clubs WHERE user_id = ? ORDER BY rating;",id)
+    p "alla todos #{@result}"
     slim(:"dina_klubbar/index", locals: {results:@result})
 end
 
@@ -63,7 +68,7 @@ post('/clubs/:id/update') do
     redirect('/clubs/index')
 end
 
-post('/clubs/:id/add') do
+post('/clubs/:id/new') do
     id = params[:id].to_i
     league_id = params[:league_id]
     db = SQLite3::Database.new('db/fotboll.db')
@@ -77,4 +82,44 @@ get('/clubs/:id/edit') do
     db.results_as_hash = true
     @result = db.execute("SELECT * FROM clubs WHERE id = ?",id).first
     slim(:"/dina_klubbar/edit", locals: {result:@result}) 
+end
+
+get('/showregister') do
+    slim(:register)
+end
+
+get('/showlogin') do
+    slim(:login)
+end
+
+post('/users/new') do
+    username = params[:username]
+    password = params[:password]
+    password_confirm = params[:password_confirm]
+
+    if (password == password_confirm)
+        password_digest = BCrypt::Password.create(password)
+        db = SQLite3::Database.new('db/fotboll.db')
+        db.execute("INSERT INTO users (pwd,username) VALUES (?,?)",password_digest, username)
+        redirect("/")
+    else
+        "Lösenord matchar inte"
+    end
+end
+
+post('/login') do
+    username = params[:username]
+    password = params[:password]
+    db = SQLite3::Database.new('db/fotboll.db')
+    db.results_as_hash = true
+    @result = db.execute("SELECT * FROM users WHERE username = ?", username).first
+    pwdigest = @result["pwd"]
+    id = @result["id"]
+
+    if BCrypt::Password.new(pwdigest) == password
+        session[:id] = id
+        redirect("/")
+    else
+        "FEL LÖSENORD"
+    end
 end
