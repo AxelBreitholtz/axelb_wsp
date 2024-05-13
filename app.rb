c5752134934fbaf407fea88cdb62c34729070a0e
AxelBreitholtzNTIJohanneberg <axel.breitholtz@elev.ga.ntig.se>
2024-05-14 01:02:55 +0200

    komp

--- app.rb
+++ app.rb
@@ -3,19 +3,25 @@ require 'sinatra'
 require 'sinatra/reloader'
 require 'sqlite3'
 require 'bcrypt'
-require_relative 'model.rb'
+require_relative './model.rb'
 
 enable :sessions
 
+include Model
+
+# Display landing page and list of leagues
+#
+# @see Model#select_all_leagues
+# @see Model#select_user_role
 get('/') do
-    id = session[:id].to_i  
-    db = SQLite3::Database.new('db/fotboll.db')
-    db.results_as_hash = true
-    @result = db.execute("SELECT * FROM leagues ORDER BY leagues.id;")
-    @user = db.execute("SELECT role FROM users WHERE id = ?", id)
-    slim(:"ligor/index", locals: {results:@result, user:@user})
+    id = session[:id].to_i
+    db = get_database()
+    @result = select_all_leagues(db)
+    @user = select_user_role(db, id)
+    slim(:"ligor/index", locals: {results: @result, user: @user})
 end
 
+# Executed before any routes matching '/protected/*'.
 before('/protected/*') do
     p "These are protected methods"
     if session[:id] == nil
@@ -23,192 +29,246 @@ before('/protected/*') do
     end
 end
 
+# Executed before any routes matching '/admin/*'.
 before('/admin/*') do
     id = session[:id].to_i
-    db = SQLite3::Database.new('db/fotboll.db')
-    if db.execute("SELECT role from users WHERE id = ?",id).to_s.include?("admin")
+    db = get_database()
+    if select_user_role(db,id).to_s.include?("admin")
     else
         p "detta är adminfunktionalitet"
         redirect("/")
     end
 end
 
-
+# Handles the GET request to display the form for creating a new club.
+#
+# @see Model#get_database
+# @see Model#select_all_leagues
 get('/protected/clubs/new') do 
     id = session[:id].to_i
-    db = SQLite3::Database.new('db/fotboll.db')
-    db.results_as_hash = true
-    @result = db.execute("SELECT * FROM leagues")
-    slim(:"klubbar/new", locals: {results:@result})
-end 
+    db = get_database()
+    @result = select_all_leagues(db)
+    slim(:"klubbar/new", locals: {results: @result})
+end
 
+
+# Handles the POST request to create a new club.
+#
+# @param [Hash] params The parameters sent with the request.
+# @param[String] klubbnamn, the clubname
+# @param [Integer] rating, the rating of the new club.
+# @param [String] league_id, id of the league which the club belongs to
+# 
+# @see Model#get_database
+# @see Model#select_clubname_where
+# @see Model#validation_input_club
+# @see Model#insert_into_clubs
+# @see Model#insert_into_leaguesclubs
 post('/protected/clubs/new') do 
-    db = SQLite3::Database.new('db/fotboll.db')
-    club_names = db.execute("SELECT club_name FROM clubs WHERE club_name = ?", params[:klubbnamn])
+    db = get_database()
+    club_names = select_clubname_where(db, params[:klubbnamn])
 
-    if params[:rating].to_i <= 10 && params[:rating].to_i >= 0 && params[:klubbnamn] != nil && club_names.length == 0
+    if validation_input_club(params[:rating], params[:klubbnamn], club_names)
         id = session[:id]
         klubbnamn = params[:klubbnamn]
         rating = params[:rating].to_i
         league_id = params[:league_id].to_i
-        db = SQLite3::Database.new('db/fotboll.db')
-        db.execute("INSERT INTO clubs (club_name,rating,user_id) VALUES (?,?,?)",klubbnamn,rating,id)
-        club_Id = db.execute("SELECT id FROM clubs ORDER BY id DESC LIMIT 1;")
-        db.execute("INSERT INTO leaguesclubs (league_id,club_id) VALUES (?,?)",league_id,club_Id)
+        insert_into_clubs(db, klubbnamn, rating, id)
+        club_id = db.last_insert_row_id
+        insert_into_leaguesclubs(db, league_id, club_id)
         redirect '/protected/clubs/new'
     else
         "Betyget måste vara mellan 1-10 eller tomt namn"
     end
 end
 
+# Displays all the clubs that are owned by the logged-in user
+#
+# @see Model#get_database
+# @see Model#select_all_clubs_where
 get('/protected/clubs/index') do
     id = session[:id].to_i
-    db = SQLite3::Database.new('db/fotboll.db')
-    db.results_as_hash = true
-    @result = db.execute("SELECT * FROM clubs WHERE user_id = ? ORDER BY rating DESC;",id)
-    slim(:"dina_klubbar/index", locals: {results:@result})
+    db = get_database()
+    @result = select_all_clubs_where(db, id)
+    slim(:"dina_klubbar/index", locals: {results: @result})
 end
 
+# Displays all clubs associated with the league id
+#
+# @param [Integer] league_id, id of the league.
+#
+# @see Model#get_database
+# @see Model#innerjoin_leaguesclubs
 get('/clubs/index/:league_id') do
-    db = SQLite3::Database.new('db/fotboll.db')
-    db.results_as_hash = true
-    sql_code = "
-    SELECT clubs.club_name, clubs.rating FROM leagues
-    INNER JOIN leaguesclubs ON leagues.id = leaguesclubs.league_id
-    INNER JOIN clubs ON clubs.id = leaguesclubs.club_id
-    WHERE leagues.id = ?
-    ORDER BY clubs.rating DESC;
-    "
-    @result = db.execute(sql_code,[params[:league_id]])
-    slim(:"klubbar/index", locals: {results:@result})
+    db = get_database()
+    @result = innerjoin_leaguesclubs(db, params[:league_id])
+    slim(:"klubbar/index", locals: {results: @result})
 end
 
-# Tar bort klubbar (ska lägga till så att man kollar att den som tar bort äger klubben dvs om inloggat id tillhör klubben)
+# Handles the deletion of a club and redirects to '/protected/clubs/index'
+#
+# @param [Integer] id, id of the club to be deleted.
+# 
+# @see Model#get_database
+# @see Model#select_userid_clubs_where
+# @see Model#delete_clubs_where
+# @see Model#delete_leagues_clubs_where
 post('/protected/clubs/:id/delete') do
     id = params[:id].to_i
-    db = SQLite3::Database.new('db/fotboll.db')
-    user_id = db.execute("SELECT user_id FROM clubs WHERE id=?",id).first
-    if user_id.first.to_i == session[:id].to_i
-        db.execute("DELETE FROM clubs WHERE id = ?",id)
-        # Tar även bort från relationstabellen
-        db.execute("DELETE FROM leaguesclubs WHERE club_id = ?",id)
+    db = get_database()
+    user_id = select_userid_clubs_where(db, id).first
+    if user_id.first[1].to_i == session[:id].to_i
+        delete_clubs_where(db, id)
+        delete_leagues_clubs_where(db, id)
     else
-        #Sinatra flash?
-        p "du äger inte denna klubben"
+        "du äger inte denna klubben"
     end
     redirect '/protected/clubs/index'
 end
 
+
+# Handles the update of a club and redirects to '/protected/clubs/index'
+#
+# @param [Integer] id, ID of the club to be updated.
+# @param [String] club_name, updated name of the club.
+# @param [Integer] rating, updated rating of the club.
+#
+# @see Model#get_database
+# @see Model#select_clubname_where
+# @see Model#select_userid_clubs_where
+# @see Model#validation_update_clubname
+# @see Model#validation_update_rating
+# @see Model#update_clubs_coalesce
 post('/protected/clubs/:id/update') do
-    db = SQLite3::Database.new('db/fotboll.db')
+    db = get_database()
     id = params[:id].to_i
     club_name = params[:club_name]
     rating = params[:rating]
+    club_names = select_clubname_where(db, club_name)
+    user_id = select_userid_clubs_where(db, id).first
 
-    club_names = db.execute("SELECT club_name FROM clubs WHERE club_name = ?", club_name)
-
-    user_id = db.execute("SELECT user_id FROM clubs WHERE id=?",id).first
-    if user_id.first.to_i == session[:id].to_i
-        if club_name && !club_name.empty?
-            existing_club_names = db.execute("SELECT club_name FROM clubs WHERE club_name = ? AND id != ?", club_name, id)
-            if existing_club_names.any?
-                return "Klubbnamnet finns redan"
-            end
+    if user_id.first[1].to_i == session[:id].to_i
+        if validation_update_clubname(db, club_name, id)
+            return "Klubbnamnet finns redan"
         end
-
-        # Check if the provided rating is within the valid range
-        if rating && !rating.empty?
-            rating_value = rating.to_i
-            if rating_value < 0 || rating_value > 10
-                return "Betyget måste vara mellan 0-10"
-            end
+        if validation_update_rating(db, rating)
+            return "Betyget måste vara mellan 0-10"
         end
-
-        # Update the club record
-        db.execute("UPDATE clubs SET club_name = COALESCE(?, club_name), rating = COALESCE(?, rating) WHERE id = ?", club_name, rating, id)
+        update_clubs_coalesce(db, club_name, rating, id)
     else
-        #Sinatra flash?
-        p "du äger inte denna klubben"
+        "du äger inte denna klubben"
     end
-
     redirect('/protected/clubs/index')
-
 end
 
+# Handles the addition of a new club to a league and redirects to '/protected/clubs/index'
+#
+# @param [Integer] id The ID of the club to be added to the league.
+# @param [Integer] league_id The ID of the league to which the club is being added.
+#
+# @see Model#get_database
+# @see Model#insert_into_leaguesclubs
 post('/protected/clubs/:id/new') do
     id = params[:id].to_i
     league_id = params[:league_id]
-    db = SQLite3::Database.new('db/fotboll.db')
-    db.execute("INSERT INTO leaguesclubs (league_id,club_id) VALUES (?,?)",league_id,id)
+    db = get_database()
+    insert_into_leaguesclubs(db, league_id, id)
     redirect('/protected/clubs/index')
 end
 
+# Renders the edit page for a specific club.
+#
+# @param [Integer] id, id of the club to be edited.
+# 
+# @see Model#get_database
+# @see Model#select_clubs_where_first
+# @see Model#select_id_name_leagues
 get('/protected/clubs/:id/edit') do
     id = params[:id].to_i
-    db = SQLite3::Database.new('db/fotboll.db')
-    db.results_as_hash = true
-    @result = db.execute("SELECT * FROM clubs WHERE id = ?",id).first
-    @league_result = db.execute("SELECT id,name from leagues")
-    slim(:"/dina_klubbar/edit", locals: {results:@result,league_results:@league_result}) 
+    db = get_database()
+    @result = select_clubs_where_first(db, id)
+    @league_result = select_id_name_leagues(db)
+    slim(:"/dina_klubbar/edit", locals: {results: @result, league_results: @league_result}) 
 end
 
+# Opening the database for creating a new league.
+#
+# @see Model#get_database
 get('/admin/ligor/new') do 
-    db = SQLite3::Database.new('db/fotboll.db')
-    db.results_as_hash = true
-    slim(:"/ligor/new", locals: {results:@result})
+    db = get_database()
+    slim(:"/ligor/new", locals: {results: @result})
 end
 
+# Handles the creation of a new league.
+#
+# @param [String] lignamn, leaguename.
+# 
+# @see Model#get_database
+# @see Model#insert_into_leagues
 post('/admin/ligor/new') do 
     lignamn = params[:leaguename]
-    db = SQLite3::Database.new('db/fotboll.db')
-    db.results_as_hash = true
-    db.execute("INSERT INTO leagues (name) VALUES (?)", lignamn)
-
-    slim(:"/ligor/new", locals: {results:@result})
+    db = get_database()
+    insert_into_leagues(db, lignamn)
+    slim(:"/ligor/new", locals: {results: @result})
 end
 
-
+# Display the registration page.
 get('/showregister') do
     slim(:register)
 end
 
+# Displays a login form
 get('/showlogin') do
     slim(:login)
 end
 
+# Handles the creation of a new user account and redirect to home page.
+#
+# @param [String] username, The new username 
+# @param [String] password, The new password
+# @param [String] password_confirm, The confirmation of the password.
 post('/users/new') do
     username = params[:username]
     password = params[:password]
     password_confirm = params[:password_confirm]
 
-    if (password == password_confirm)
-        password_digest = BCrypt::Password.create(password)
-        db = SQLite3::Database.new('db/fotboll.db')
-        db.execute("INSERT INTO users (pwd,username) VALUES (?,?)",password_digest, username)
+    if password_input_check(password, password_confirm)
+        password_digest = create_password(password)
+        db = get_database()
+        register_user(db, password_digest, username)
         redirect("/")
     else
         "Lösenord matchar inte"
     end
 end
 
+# Handles the user login process and redirect to homepage after succesful login.
+#
+# @param [String] username, The username 
+# @param [String] password, The password 
+# 
+# @see Model#get_database
+# @see Model#select_users_where_first
+# @see Model#login_cooldown_expired
+# @see Model#password_check
+# @see Model#set_login_attempt
 post('/login') do
     username = params[:username]
     password = params[:password]
-    db = SQLite3::Database.new('db/fotboll.db')
-    db.results_as_hash = true
+    db = get_database()
 
-    @result = db.execute("SELECT * FROM users WHERE username = ?", username).first
-
-    if @result.nil?
+    @result = select_users_where_first(db, username)
+    if user_not_exist?(@result)
         set_login_attempt(username)
         "Invalid Credentials"
-        redirect '/showlogin' #kanske nåt error vi får se
+        redirect '/showlogin' 
     end
     
     pwdigest = @result["pwd"]
     id = @result["id"]
+    
     if login_cooldown_expired(username)
-        if BCrypt::Password.new(pwdigest) == password
+        if password_check(pwdigest, password)
             session.delete(:login_attempts)
             session[:id] = id
             redirect("/")
@@ -221,47 +281,46 @@ post('/login') do
     end
 end
 
+# Logout user and redirect to login form
 post('/logout') do
     session[:id] = nil
     redirect '/showlogin'
 end
 
-# VÄLDIGT FUL KOD 
+# Upload a club emblem and redirect to /protected/clubs/index.
+#
+# @param [Hash] params, The parameters
+# 
+# @see Model#get_database
+# @see Model#file_exist?
+# @see Model#write_file
+# @see Model#update_club_emblem
 post('/protected/:id/upload_image') do
-    if params[:file] && params[:file][:filename]
+    if file_exist?(params[:file], params[:file][:filename])
         filename = params[:file][:filename]
         file = params[:file][:tempfile]
         path = "./public/uploads/#{filename}"
 
-        File.open(path,'wb') do |image|
-            image.write(file.read)
-        end
+        write_file(path, file)
 
         club_id = params[:id]
-        p "detta är club id"
-        p club_id
-        db = SQLite3::Database.new('db/fotboll.db')
-        db.execute("UPDATE clubs SET club_emblem=? WHERE id = ?",filename,club_id)
+        db = get_database()
+        update_club_emblem(db, filename, club_id)
     end
     redirect '/protected/clubs/index'
 end 
 
-#funktioner som jag inte lyckas kalla från model.rb!!!
-
+# Checks if the login cooldown period has expired.
+#
+# @param [String] username, The username which the cooldown is checked.
 def login_cooldown_expired(username)
-    max_login_attempts = 3  
-    cooldown_time = 10
-    last_attempt_time = session[:login_attempts]&.fetch(username, nil) # & för att ange att andra värdet kan vara nil och fetch för att få tag på hash
-    if last_attempt_time.nil?
-        return true
-    end 
-    if Time.now - last_attempt_time > cooldown_time 
-        return true
-    else
-        return false
-    end
+    last_attempt_time = session[:login_attempts]&.fetch(username, nil)
+    check_last_attempt_time(last_attempt_time)
 end
 
+# Sets the login attempt time for a given username.
+#
+# @param [String] username, The username which the login attempt time is set for .
 def set_login_attempt(username)
     if session[:login_attempts].nil?
         session[:login_attempts] = {}
--- model.rb
+++ model.rb
@@ -1,6 +1,277 @@
-def get_database()
-    db = SQLite3::Database.new('db/fotboll.db')
-    db.results_as_hash = true
-    return db
-end
+module Model
+    # Establishes a connection to the SQLite database.
+    #
+    # @return [SQLite3::Database] The SQLite database connection.   
+    def get_database()
+        db = SQLite3::Database.new('db/fotboll.db')
+        db.results_as_hash = true
+        return db
+    end
 
+    # Creates a hashed representation of the given password using bcrypt.
+    #
+    # @param [String] password The password to be hashed.
+    # @return [String] The hashed password.
+    def create_password(password)
+        return BCrypt::Password.create(password)
+    end
+
+    # Creates a BCrypt::Password instance from the given password digest.
+    #
+    # @param [String] pwdigest The password digest to create the BCrypt::Password instance from.
+    # @return [BCrypt::Password] The BCrypt::Password instance.
+    def salt_password(pwdigest)
+        return BCrypt::Password.new(pwdigest)
+    end
+
+    # Selects all leagues from the database.
+    #
+    # @param [SQLite3::Database] db The database connection.
+    # @return [Array<Array>] An array containing the results of the query.
+    def select_all_leagues(db)
+        return db.execute("SELECT * FROM leagues ORDER BY leagues.id;")
+    end
+
+    # Selects the role of the user with the given ID from the database.
+    #
+    # @param [SQLite3::Database] db The database connection.
+    # @param [Integer] id The ID of the user.
+    # @return [Array<Array>] An array containing the results of the query.
+    def select_user_role(db, id)
+        return db.execute("SELECT role FROM users WHERE id = ?", id)
+    end
+
+    # Selects the club name from the database where club_name matches the given name.
+    #
+    # @param [SQLite3::Database] db The database connection.
+    # @param [String] club_name The name of the club.
+    # @return [Array<Array>] An array containing the results of the query.
+    def select_clubname_where(db, club_name)
+        return db.execute("SELECT club_name FROM clubs WHERE club_name = ?", club_name)
+    end
+
+    # Inserts a new club into the clubs table.
+    #
+    # @param [SQLite3::Database] db The database connection.
+    # @param [String] klubbnamn The name of the club.
+    # @param [Integer] rating The rating of the club.
+    # @param [Integer] id The ID of the user.
+    def insert_into_clubs(db, klubbnamn, rating, id)
+        db.execute("INSERT INTO clubs (club_name, rating, user_id) VALUES (?, ?, ?)", klubbnamn, rating, id)
+    end
+
+    # Inserts a new record into the leaguesclubs table.
+    #
+    # @param [SQLite3::Database] db The database connection.
+    # @param [Integer] league_id The ID of the league.
+    # @param [Integer] club_id The ID of the club.
+    def insert_into_leaguesclubs(db, league_id, club_id)
+        db.execute("INSERT INTO leaguesclubs (league_id, club_id) VALUES (?, ?)", league_id, club_id)
+    end
+
+    # Selects all clubs associated with the given user ID from the database.
+    #
+    # @param [SQLite3::Database] db The database connection.
+    # @param [Integer] id The ID of the user.
+    # @return [Array<Array>] An array containing the results of the query.
+    def select_all_clubs_where(db, id)
+        db.execute("SELECT * FROM clubs WHERE user_id = ? ORDER BY rating DESC;", id)
+    end
+
+    # Performs an inner join on the leaguesclubs and clubs tables to select clubs associated with the given league ID.
+    #
+    # @param [SQLite3::Database] db The database connection.
+    # @param [Integer] league_id The ID of the league.
+    # @return [Array<Array>] An array containing the results of the query.
+    def innerjoin_leaguesclubs(db, league_id)
+        sql_code = "
+        SELECT clubs.club_name, clubs.rating FROM leagues
+        INNER JOIN leaguesclubs ON leagues.id = leaguesclubs.league_id
+        INNER JOIN clubs ON clubs.id = leaguesclubs.club_id
+        WHERE leagues.id = ?
+        ORDER BY clubs.rating DESC;
+        "
+        return db.execute(sql_code, league_id)
+    end
+
+    # Selects the user ID associated with the given club ID from the database.
+    #
+    # @param [SQLite3::Database] db The database connection.
+    # @param [Integer] id The ID of the club.
+    # @return [Array<Array>] An array containing the results of the query.
+    def select_userid_clubs_where(db, id)
+        return db.execute("SELECT user_id FROM clubs WHERE id=?", id)
+    end
+
+    # Deletes a club from the clubs table based on the given ID.
+    #
+    # @param [SQLite3::Database] db The database connection.
+    # @param [Integer] id The ID of the club to delete.
+    def delete_clubs_where(db, id)
+        db.execute("DELETE FROM clubs WHERE id = ?", id)
+    end
+
+    # Deletes records from the leaguesclubs table where the club ID matches the given ID.
+    #
+    # @param [SQLite3::Database] db The database connection.
+    # @param [Integer] id The ID of the club.
+    def delete_leagues_clubs_where(db, id)
+        db.execute("DELETE FROM leaguesclubs WHERE club_id = ?", id)
+    end
+
+    # Checks if a club with the given name and ID exists in the database.
+    #
+    # @param [SQLite3::Database] db The database connection.
+    # @param [String] club_name The name of the club.
+    # @param [Integer] id The ID of the club.
+    # @return [Array<Array>] An array containing the results of the query.
+    def club_exist?(db, club_name, id)
+        db.execute("SELECT club_name FROM clubs WHERE club_name = ? AND id != ?", club_name, id)
+    end
+
+    # Updates the club name and rating in the clubs table based on the given ID.
+    #
+    # @param [SQLite3::Database] db The database connection.
+    # @param [String] club_name The new name of the club.
+    # @param [Integer] rating The new rating of the club.
+    # @param [Integer] id The ID of the club to update.
+    def update_clubs_coalesce(db, club_name, rating, id)
+        db.execute("UPDATE clubs SET club_name = COALESCE(?, club_name), rating = COALESCE(?, rating) WHERE id = ?", club_name, rating, id)
+    end
+
+
+        # Selects the first club record from the clubs table based on the given ID.
+    #
+    # @param [SQLite3::Database] db The database connection.
+    # @param [Integer] id The ID of the club.
+    # @return [Hash, nil] The first club record if found, nil otherwise.
+    def select_clubs_where_first(db, id)
+        return db.execute("SELECT * FROM clubs WHERE id = ?", id).first
+    end
+
+    # Selects the IDs and names of all leagues from the database.
+    #
+    # @param [SQLite3::Database] db The database connection.
+    # @return [Array<Array>] An array containing pairs of league ID and name.
+    def select_id_name_leagues(db)
+        db.execute("SELECT id, name FROM leagues")
+    end
+
+    # Inserts a new league into the leagues table.
+    #
+    # @param [SQLite3::Database] db The database connection.
+    # @param [String] lignamn The name of the league to be inserted.
+    def insert_into_leagues(db, lignamn)
+        db.execute("INSERT INTO leagues (name) VALUES (?)", lignamn)
+    end
+
+    # Register a new user into the users table.
+    #
+    # @param [SQLite3::Database] db The database connection.
+    # @param [String] password_digest The hashed password of the user.
+    # @param [String] username The username of the user.
+    def register_user(db, password_digest, username)
+        db.execute("INSERT INTO users (pwd, username) VALUES (?, ?)", password_digest, username)
+    end
+
+    # Selects the first user record from the users table based on the given username.
+    #
+    # @param [SQLite3::Database] db The database connection.
+    # @param [String] username The username of the user.
+    # @return [Hash, nil] The first user record if found, nil otherwise.
+    def select_users_where_first(db, username)
+        return db.execute("SELECT * FROM users WHERE username = ?", username).first
+    end
+
+    # Updates the emblem filename of a club in the clubs table based on the given club ID.
+    #
+    # @param [SQLite3::Database] db The database connection.
+    # @param [String] filename The filename of the emblem.
+    # @param [Integer] club_id The ID of the club.
+    def update_club_emblem(db, filename, club_id)
+        db.execute("UPDATE clubs SET club_emblem = ? WHERE id = ?", filename, club_id)
+    end
+
+    # Validates the input data for creating a new club.
+    #
+    # @param [String] rating The rating of the club.
+    # @param [String] klubbnamn The name of the club.
+    # @param [Array<String>] club_names The existing club names.
+    # @return [Boolean] true if input is valid, false otherwise.
+    def validation_input_club(rating, klubbnamn, club_names)
+        return rating.to_i.between?(0, 10) && klubbnamn != nil && club_names.empty?
+    end
+
+    # Validates the update of club name.
+    #
+    # @param [SQLite3::Database] db The database connection.
+    # @param [String] club_name The name of the club.
+    # @param [Integer] id The ID of the club.
+    # @return [Boolean] true if club name is valid for update, false otherwise.
+    def validation_update_clubname(db, club_name, id)
+        return !club_name.nil? && club_exist?(db, club_name, id)
+    end
+
+    # Validates the update of club rating.
+    #
+    # @param [SQLite3::Database] db The database connection.
+    # @param [String] rating The rating of the club.
+    # @return [Boolean] true if rating is valid for update, false otherwise.
+    def validation_update_rating(db, rating)
+        return !rating.nil? && rating.to_i.between?(0, 10)
+    end
+
+    # Checks if the password and password confirmation match.
+    #
+    # @param [String] password The password.
+    # @param [String] password_confirm The password confirmation.
+    # @return [Boolean] true if passwords match, false otherwise.
+    def password_input_check(password, password_confirm)
+        return password == password_confirm
+    end
+
+    # Checks if a user list is empty, indicating that the user does not exist.
+    #
+    # @param [Array] user_list The list of users.
+    # @return [Boolean] true if user does not exist, false otherwise.
+    def user_not_exist?(user_list)
+        return user_list.nil?
+    end
+
+    # Checks if the given password matches the hashed password.
+    #
+    # @param [String] pwdigest The hashed password.
+    # @param [String] password The password to be checked.
+    # @return [Boolean] true if passwords match, false otherwise.
+    def password_check(pwdigest, password)
+        return salt_password(pwdigest) == password
+    end
+
+    # Checks if both the file and filename exist.
+    #
+    # @param [Object] file The file object.
+    # @param [String] filename The filename.
+    # @return [Boolean] true if both file and filename exist, false otherwise.
+    def file_exist?(file, filename)
+        return file && filename
+    end
+
+    def check_last_attempt_time(last_attempt_time)
+        max_login_attempts = 3  
+        cooldown_time = 10
+        if last_attempt_time.nil?
+            return true
+        end 
+        if Time.now - last_attempt_time > cooldown_time 
+            return true
+        else
+            return false
+        end
+    end
+
+    def write_file(path,file)
+        File.open(path,'wb') do |image|
+            image.write(file.read)
+        end
+    end
+end
